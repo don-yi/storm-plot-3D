a7f9696e2213849719b7dab57ba47e6fe96ec117
Dennis <dennis.donguk.lee@gmail.com>
2020-11-29 09:36:08 +0900

    example code from dr. thomas

---
+++ example/HurricaneLauraPlotExampleDPR.m
@@ -0,0 +1,19 @@
+
+
+
+STORM_NAME='Laura';
+
+wwlln_file_name='ATL_20_13_Laura_WWLLN_Locations.txt';
+
+FILE_NAME='2A.GPM.DPR.V820180723.20200827-S024128-E031127.V06A.RT-H5';
+
+FILE_NAME_1C='1C-R.GPM.GMI.XCAL2016-C.20200827-S022853-E040126.036907.V05A.HDF5';
+
+stormCenterCoords=[29.8,-93.3];
+
+output_path='C:\Users\DennisLee\Downloads\Hurricane Laura 2020\out';
+
+output_base_filename='Laura_test';
+
+PlotStormDPR(STORM_NAME, wwlln_file_name, FILE_NAME, FILE_NAME_1C, stormCenterCoords, output_path, output_base_filename)
+    
